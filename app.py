7b2049e07238f1cc158101cd9bf13cd444201cd8
unknown <https://github.com/bhaktiagrawal088/CAP770-DSA.git>
2024-04-24 18:10:30 +0530

    BrainTumor

---
+++ app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+import cv2
+from PIL import Image, ImageOps
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+    model = tf.keras.models.load_model('/content/best_model.hdf5')
+    return model
+
+model = load_model()
+
+st.write("""
+        # Brain Tumor Detection
+        """)
+
+file = st.file_uploader("Please upload a brain image", type=['jpg', 'png'])
+
+def import_and_predict(image_data, model):
+<<<<<<< HEAD
+  size = (180, 180)
+  image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+  img = np.asarray(image)
+   prediction = model.predict(img_reshape)
+
+  return prediction
+=======
+    size = (180, 180)
+    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    img = np.asarray(image)
+    img_reshape = img[np.newaxis,...]
+    prediction = model.predict(img_reshape)
+    return prediction
+>>>>>>> 7cadf0c06044f140b75471ed9881a7e3d8288d49
+
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    predictions = import_and_predict(image, model)
+    class_names = ['Brain Tumor', 'Healthy']
+    result = class_names[np.argmax(predictions)]
+    st.success("The MRI image is of " + result)
