5300970a64aa9585023ad2942ec15bda3d90d686
Bhakti Agrawal <72144252+bhaktiagrawal088@users.noreply.github.com>
2024-04-24 18:24:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,14 +27,12 @@ def import_and_predict(image_data, model):
    prediction = model.predict(img_reshape)
 
   return prediction
-=======
     size = (180, 180)
     image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
     img = np.asarray(image)
     img_reshape = img[np.newaxis,...]
     prediction = model.predict(img_reshape)
     return prediction
->>>>>>> 7cadf0c06044f140b75471ed9881a7e3d8288d49
 
 if file is None:
     st.text("Please upload an image file")
